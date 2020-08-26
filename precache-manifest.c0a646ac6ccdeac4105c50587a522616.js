6a6d1d43a2d3d135a1577839cb36d67b73cd368f
amarjeetprasad <56919207+amarjeetprasad@users.noreply.github.com>
2020-08-27 00:39:25 +0530

    Updates

---
+++ precache-manifest.c0a646ac6ccdeac4105c50587a522616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7807ed27ca3fa6ea1b4c62fd68d1d2a",
+    "url": "/about-me/index.html"
+  },
+  {
+    "revision": "ec71bcb15bb32c3cc668",
+    "url": "/about-me/static/css/main.c5523cb3.chunk.css"
+  },
+  {
+    "revision": "137e0b811d022caa0675",
+    "url": "/about-me/static/js/2.b7f3a2c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/about-me/static/js/2.b7f3a2c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec71bcb15bb32c3cc668",
+    "url": "/about-me/static/js/main.0eb9b0aa.chunk.js"
+  },
+  {
+    "revision": "bc2da0bd953b1e5adcd9",
+    "url": "/about-me/static/js/runtime-main.1f0907cf.js"
+  }
+]);
